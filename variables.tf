2956d1f4435b80dda402842b080ac997be18993e
EduArn <eduarnofficial@gmail.com>
2025-07-27 12:25:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+   default = "galvanic-flame-466111-q8"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+output "bucket_url" {
+  value = "gs://${google_storage_bucket.example.name}"
+}
