0d92435201d4abbbd9c28e03d38fde642c8dc054
EduArn <eduarnofficial@gmail.com>
2025-07-27 12:55:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,3 @@ provider "google" {
   project     = var.project_id
   region      = var.region
 }
-
-#tf-state-prod-bykumar need to create manully
-terraform {
-  required_version = ">= 0.13"
-
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
-
-  backend "gcs" {
-    bucket      = "tf-state-prod-bykumar"
-    prefix      = "dev"
-    credentials = "~/sec.json"
-   }
-}
